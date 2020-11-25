fdc5588dc916afbc86d85cf311d90728d5c06b36
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-11-25 11:57:07 +0000

    Add plugin file and build using cmake

---
+++ build/CMakeFiles/welcome_message.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/welcome_message.dir/script/welcome_message.cpp.o"
+  "libwelcome_message.pdb"
+  "libwelcome_message.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/welcome_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
