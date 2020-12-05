7134bd60fdf299d2d01cdf40055f0d1cd2d5ebf9
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-12-05 02:53:56 +0000

    Visualize particles

--- main.cpp
+++ main.cpp
@@ -296,6 +296,8 @@ int main()
         //####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 
         //TODO: Graph the position of the robot and the particles at each step
+	visualization(n, myrobot, t, p2, p3);
+	
 
     } //End of Steps loop
 
