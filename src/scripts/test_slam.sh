c73d1501f0490343ee217d274d5b82203a3eb6d9
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-12-21 03:01:30 +0000

    Add SLAM test shell script

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -3,6 +3,12 @@ xterm -e "cd $(pwd)/../..;
 source devel/setup.bash;
 roslaunch my_robot world.launch " &
 
+sleep 5
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
 sleep 10
 
 xterm -e "cd $(pwd)/../..;
