55d6b1b1fccd5ab328890ee13d9cd35eb7dd3bb4
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-12-23 20:10:13 +0000

    Create a shell script to run all nodes for home service robot

---
+++ src/scripts/home_service.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch my_robot world.launch " &
+
+sleep 15
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_gazebo amcl.launch " &
+
+sleep 10
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
+sleep 15
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+rosrun pick_objects pick_objects " &
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+rosrun add_markers add_markers " &
